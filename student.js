78275eee35838f4d540636044f5e09bb8d6b1083
Lijun Wang <wang283808065@gmail.com>
2017-03-06 23:12:02 -0800

    A search mechanism
    Use the prompt method to open a dialog box and request the name of a student. When a student name is typed into the box, the program would search through the student records and look for the name that matches. If there's a match, then print out the student related data. And the prompt appears over and over again until the word 'quit' is entered into the prompt dialog box, the program ends.

--- student.js
+++ student.js
@@ -13,47 +13,21 @@ function print(message) {
 }
 
 var message = '';
+var nameInput = prompt("Search student records: type a name [Joan] (or type \"quit\" to end))");
 
-for (var i = 0; i < student.length; i += 1){
+while (true){
+  if (nameInput === null || nameInput.toLocaleLowerCase() === 'quit'){
+    break;
+  }
+  for (var i = 0; i < student.length; i += 1){
+    if ()
   message += ('<h2>Student: ' + student[i].Name + '</h2>');
   message += ('<p>Track: ' + student[i].Track + '</p>');
   message += ('<p>Points: ' + student[i].Points + '</p>');
   message += ('<p>Achivements: ' + student[i].Achivements + '</p>');
 }
 print(message);
-
-
-
-/////////////////////////////////////////////////////
-var student = [
-  {Name: 'Jodan', Track: 'JavaScript', Achivements: 'course1', Points: 150},
-  {Name: 'Xiao', Track: 'Java', Achivements: 'course3', Points: 111},
-  {Name: 'Dave', Track: 'iOS', Achivements: 'course5', Points: 120},
-  {Name: 'Jody', Track: 'Web design', Achivements: 'course8', Points: 123},
-  {Name: 'Lijun', Track: 'Startup', Achivements: 'course2', Points: 1120}
-];
-
-function print(message) {
-//  var outputDiv = ducument.getElementById("output");
-//  outputDiv.innerHTML = message;
-  document.write(message);
 }
 
-var message = '';
-var nameInput = prompt("Search student records: type a name [Joan] (or type \"quit\" to end))");
-var i = 0;
 
-while (true){
-  if (nameInput === null || nameInput.toLocaleLowerCase() === 'quit'){
-    break;
-  }
-  if (nameInput === student[i].Name){
-    message += ('<h2>Student: ' + student[i].Name + '</h2>');
-    message += ('<p>Track: ' + student[i].Track + '</p>');
-    message += ('<p>Points: ' + student[i].Points + '</p>');
-    message += ('<p>Achivements: ' + student[i].Achivements +     '</p>');
-  }
-  i++;
-}
 
-print(message);
